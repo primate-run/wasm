12077b2f694b3e14dee012d65e9f66ee387383dc
terrablue <102580937+terrablue@users.noreply.github.com>
2023-10-29 17:01:57 +0200

    initial commit

---
+++ index.zig
@@ -0,0 +1,6 @@
+extern fn respond_string(pointer: [*]const u8, length: u32) void;
+
+pub export fn get() void {
+    const response : []const u8 = "Hi!";
+    respond_string(response.ptr, response.len);
+}
