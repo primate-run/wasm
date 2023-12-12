197aa671470370542d0d5458e9f7000bb272681a
terrablue <102580937+terrablue@users.noreply.github.com>
2023-12-13 01:22:41 +0200

    rename

--- ruby/index.js
+++ ruby/index.js
@@ -2,7 +2,7 @@ import { Path } from "rcompat/fs";
 import { DefaultRubyVM } from "./node.esm.js";
 
 const bin = await new Path("./ruby.wasm").arrayBuffer();
-const get = await new Path("./get.rb").text();
+const get = await new Path("./index.rb").text();
 const module = await WebAssembly.compile(bin);
 const { vm } = await DefaultRubyVM(module);
 const t = await vm.eval(`
